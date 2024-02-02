bcad9fd861907718a9ff08e0ddd6ca173f123a73
alhammanazil <alhammanazil18@gmail.com>
2024-02-02 15:01:56 +0700

    delete data based on filter or id with mongoose

--- index.js
+++ index.js
@@ -8,43 +8,20 @@ const movieSchema = new mongoose.Schema({
     title: String,
     year: Number,
     genre: String,
-    director: String,
-    rating: Number,
-    cast: [String],
-    image: String
+    rating: Number
 });
 
 // MOVIE MODEL
 const Movie = mongoose.model('Movie', movieSchema);
 
-// FIND BY ID
-// Movie.findById('65bc6152b47040372a3a3453')
+// DELETE
+// Movie.deleteOne({ title: 'Avengers: Infinity War' })
 // .then((result) => {
-//     console.log('Movie found:', result);
+//     console.log('Movie deleted:', result);
 // })
 // .catch((err) => {
 //     console.log('error', err);
 // });
-
-
-
-// UPDATE DATA
-// Movie.updateMany({ year: {$lt: 2019 }}, {rating: 7})
-// .then((result) => {
-//     console.log('Movie updated:', result);
-// })
-// .catch((err) => {
-//     console.log('error', err);
-// });
-
-
-Movie.findByIdAndUpdate('65bc67808aed261a596c317f', {rating: 9})
-.then((result) => {
-    console.log('Movie updated:', result);
-})
-.catch((err) => {
-    console.log('error', err);
-});
 
 
 // FIND MOVIE BY YEAR
@@ -57,52 +34,47 @@ Movie.findByIdAndUpdate('65bc67808aed261a596c317f', {rating: 9})
 // });
 
 
+Movie.findByIdAndDelete('65bc9fdfca2a72925ddada44')
+ .then((result) => {
+    console.log('Movie deleted:', result);
+})
+
+.catch((err) => {
+    console.log('error', err);
+});
+
+
 
 // Movie.insertMany([
 //         {
 //             "title": "Black Panther",
 //             "genre": "Action",
-//             "director": "Ryan Coogler",
 //             "year": 2018,
-//             "cast": ["Chadwick Boseman", "Michael B. Jordan", "Lupita Nyong'o"],
-//             "rating": 7.3,
-//             "image": "https://www.example.com/black_panther.jpg"
+//             "rating": 7.3
 //         },
 //         {
 //             "title": "Avengers: Infinity War",
 //             "genre": "Action",
-//             "director": "Anthony Russo, Joe Russo",
 //             "year": 2018,
-//             "cast": ["Robert Downey Jr.", "Chris Hemsworth", "Mark Ruffalo"],
-//             "rating": 8.4,
-//             "image": "https://www.example.com/avengers_infinity_war.jpg"
+//             "rating": 8.4
 //         },
 //         {
 //             "title": "Joker",
 //             "genre": "Crime",
-//             "director": "Todd Phillips",
 //             "year": 2019,
-//             "cast": ["Joaquin Phoenix", "Robert De Niro", "Zazie Beetz"],
-//             "rating": 8.4,
-//             "image": "https://www.example.com/joker.jpg"
+//             "rating": 8.4
 //         },
 //         {
 //             "title": "Parasite",
 //             "genre": "Drama",
-//             "director": "Bong Joon Ho",
 //             "year": 2019,
-//             "cast": ["Song Kang-ho", "Lee Sun-kyun", "Cho Yeo-jeong"],
-//             "rating": 8.6,
-//             "image": "https://www.example.com/parasite.jpg"
+//             "rating": 8.6
 //         },
 //         {
 //             "title": "Spider-Man: Into the Spider-Verse",
 //             "genre": "Animation",
-//             "director": "Bob Persichetti, Peter Ramsey, Rodney Rothman",
 //             "year": 2018,
-//             "cast": ["Shameik Moore", "Jake Johnson", "Hailee Steinfeld"],
-//             "rating": 8.4,
-//             "image": "https://www.example.com/spider_man_into_the_spider_verse.jpg"
+//             "rating": 8.4
 //         }
 //     ])
 //     .then((result) => {
