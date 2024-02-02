2abcb330b8e21c07ccc73b0fad89b7617dfb3cde
alhammanazil <alhammanazil18@gmail.com>
2024-02-02 11:15:37 +0700

    change data by filter or id with mongoose

--- index.js
+++ index.js
@@ -11,15 +11,14 @@ const movieSchema = new mongoose.Schema({
     director: String,
     rating: Number,
     cast: [String],
-    description: String,
     image: String
 });
 
 // MOVIE MODEL
 const Movie = mongoose.model('Movie', movieSchema);
 
-// FIND MOVIE BY ID
-// Movie.findOne({ _id: '65bc6152b47040372a3a3453' })
+// FIND BY ID
+// Movie.findById('65bc6152b47040372a3a3453')
 // .then((result) => {
 //     console.log('Movie found:', result);
 // })
@@ -27,14 +26,26 @@ const Movie = mongoose.model('Movie', movieSchema);
 //     console.log('error', err);
 // });
 
-// FIND BY ID
-Movie.findById('65bc6152b47040372a3a3453')
+
+
+// UPDATE DATA
+// Movie.updateMany({ year: {$lt: 2019 }}, {rating: 7})
+// .then((result) => {
+//     console.log('Movie updated:', result);
+// })
+// .catch((err) => {
+//     console.log('error', err);
+// });
+
+
+Movie.findByIdAndUpdate('65bc67808aed261a596c317f', {rating: 9})
 .then((result) => {
-    console.log('Movie found:', result);
+    console.log('Movie updated:', result);
 })
 .catch((err) => {
     console.log('error', err);
 });
+
 
 // FIND MOVIE BY YEAR
 // Movie.findOne({ year: {$gt: 2018}})
@@ -43,63 +54,60 @@ Movie.findById('65bc6152b47040372a3a3453')
 // })
 // .catch((err) => {
 //     console.log('error', err);
-// }); 
+// });
 
-Movie.insertMany([
-        {
-            "title": "Black Panther",
-            "genre": "Action",
-            "director": "Ryan Coogler",
-            "year": 2018,
-            "cast": ["Chadwick Boseman", "Michael B. Jordan", "Lupita Nyong'o"],
-            "description": "T'Challa, the King of Wakanda, rises to the throne in the isolated, technologically advanced African nation, but his claim is challenged by a vengeful outsider who was a childhood victim of T'Challa's father's mistake.",
-            "rating": 7.3,
-            "image": "https://www.example.com/black_panther.jpg"
-        },
-        {
-            "title": "Avengers: Infinity War",
-            "genre": "Action",
-            "director": "Anthony Russo, Joe Russo",
-            "year": 2018,
-            "cast": ["Robert Downey Jr.", "Chris Hemsworth", "Mark Ruffalo"],
-            "description": "The Avengers and their allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos before his blitz of devastation and ruin puts an end to the universe.",
-            "rating": 8.4,
-            "image": "https://www.example.com/avengers_infinity_war.jpg"
-        },
-        {
-            "title": "Joker",
-            "genre": "Crime",
-            "director": "Todd Phillips",
-            "year": 2019,
-            "cast": ["Joaquin Phoenix", "Robert De Niro", "Zazie Beetz"],
-            "description": "In Gotham City, mentally-troubled comedian Arthur Fleck is disregarded and mistreated by society. He then embarks on a downward spiral of revolution and bloody crime. This path brings him face-to-face with his alter-ego: 'The Joker'.",
-            "rating": 8.4,
-            "image": "https://www.example.com/joker.jpg"
-        },
-        {
-            "title": "Parasite",
-            "genre": "Drama",
-            "director": "Bong Joon Ho",
-            "year": 2019,
-            "cast": ["Song Kang-ho", "Lee Sun-kyun", "Cho Yeo-jeong"],
-            "description": "Greed and class discrimination threaten the newly formed symbiotic relationship between the wealthy Park family and the destitute Kim clan.",
-            "rating": 8.6,
-            "image": "https://www.example.com/parasite.jpg"
-        },
-        {
-            "title": "Spider-Man: Into the Spider-Verse",
-            "genre": "Animation",
-            "director": "Bob Persichetti, Peter Ramsey, Rodney Rothman",
-            "year": 2018,
-            "cast": ["Shameik Moore", "Jake Johnson", "Hailee Steinfeld"],
-            "description": "Teen Miles Morales becomes the Spider-Man of his universe, and must join with five spider-powered individuals from other dimensions to stop a threat for all realities.",
-            "rating": 8.4,
-            "image": "https://www.example.com/spider_man_into_the_spider_verse.jpg"
-        }
-    ])
-    .then((result) => {
-        console.log('it works...');
-    })
-    .catch((err) => {
-        console.log('error', err);
-    });
+
+
+// Movie.insertMany([
+//         {
+//             "title": "Black Panther",
+//             "genre": "Action",
+//             "director": "Ryan Coogler",
+//             "year": 2018,
+//             "cast": ["Chadwick Boseman", "Michael B. Jordan", "Lupita Nyong'o"],
+//             "rating": 7.3,
+//             "image": "https://www.example.com/black_panther.jpg"
+//         },
+//         {
+//             "title": "Avengers: Infinity War",
+//             "genre": "Action",
+//             "director": "Anthony Russo, Joe Russo",
+//             "year": 2018,
+//             "cast": ["Robert Downey Jr.", "Chris Hemsworth", "Mark Ruffalo"],
+//             "rating": 8.4,
+//             "image": "https://www.example.com/avengers_infinity_war.jpg"
+//         },
+//         {
+//             "title": "Joker",
+//             "genre": "Crime",
+//             "director": "Todd Phillips",
+//             "year": 2019,
+//             "cast": ["Joaquin Phoenix", "Robert De Niro", "Zazie Beetz"],
+//             "rating": 8.4,
+//             "image": "https://www.example.com/joker.jpg"
+//         },
+//         {
+//             "title": "Parasite",
+//             "genre": "Drama",
+//             "director": "Bong Joon Ho",
+//             "year": 2019,
+//             "cast": ["Song Kang-ho", "Lee Sun-kyun", "Cho Yeo-jeong"],
+//             "rating": 8.6,
+//             "image": "https://www.example.com/parasite.jpg"
+//         },
+//         {
+//             "title": "Spider-Man: Into the Spider-Verse",
+//             "genre": "Animation",
+//             "director": "Bob Persichetti, Peter Ramsey, Rodney Rothman",
+//             "year": 2018,
+//             "cast": ["Shameik Moore", "Jake Johnson", "Hailee Steinfeld"],
+//             "rating": 8.4,
+//             "image": "https://www.example.com/spider_man_into_the_spider_verse.jpg"
+//         }
+//     ])
+//     .then((result) => {
+//         console.log('it works...');
+//     })
+//     .catch((err) => {
+//         console.log('error', err);
+//     });
