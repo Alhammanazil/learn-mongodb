699dbc004a399cc7fa32c400d8dfcf2531531aa9
alhammanazil <107827210+Alhammanazil@users.noreply.github.com>
2024-02-04 08:28:53 +0700

    make static method for models

--- products.js
+++ products.js
@@ -3,55 +3,54 @@ mongoose.connect('mongodb://127.0.0.1:27017/ShopApp')
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB...', err));
 
-
 // PRODUCT SCHEMA
 const productSchema = new mongoose.Schema({
-		name: {
-            type: String,
+    name: {
+        type: String,
+        required: true
+    },
+    brand: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    color: {
+        type: String,
+        required: true
+    },
+    size: {
+        type: [String],
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    condition: {
+        type: String,
+        enum: ['baru', 'bekas'],
+        default: 'baru',
+        required: true
+    },
+    stock: {
+        type: Number,
+        required: true,
+        min: [0, 'Stock tidak boleh kurang dari 0']
+    },
+    availability: {
+        online: {
+            type: Boolean,
             required: true
         },
-        brand: {
-            type: String,
+        offline: {
+            type: Boolean,
             required: true
-        },
-        price: {
-            type: Number,
-            required: true,
-            min: 0
-        },
-        color: {
-            type: String,
-            required: true
-        },
-        size: {
-            type: [String],
-            required: true
-        },
-        description: {
-            type: String,
-            required: true
-        },
-        condition: {
-            type: String,
-            enum: ['baru', 'bekas'],
-            default: 'baru',
-            required: true
-        },
-        stock: {
-            type: Number,
-            required: true,
-            min: [0, 'Stock tidak boleh kurang dari 0']
-        },
-        availability: {
-            online: {
-                type: Boolean,
-                required: true
-            },
-            offline: {
-                type: Boolean,
-                required: true
-            }
         }
+    }
 });
 
 productSchema.methods.outStock = function() {
@@ -61,56 +60,26 @@ productSchema.methods.outStock = function() {
     return this.save();
 }
 
+productSchema.statics.closeStore = function() {
+    return this.updateMany({}, {
+        stock: 0,
+        "availability.online": false,
+        "availability.offline": false,
+    });
+}
+
 const Product = mongoose.model('Product', productSchema);
+
+Product.closeStore()
+    .then((result) => {
+        console.log('Berhasil ditutup', result);
+    })
+    .catch((err) => {
+        console.log('Gagal ditutup', err);
+    });
 
 const changeStock = async (id) => {
     const foundProduct = await Product.findById(id)
     await foundProduct.outStock();
     console.log(`Berhasil Diubah! ${foundProduct.name} stock: ${foundProduct.stock}`)
 }
-
-changeStock('65becdeb5aa36727c6e5ff8f');
-
-// const product = new Product({
-// 		"name": "Kemeja Flanel",
-// 		"brand": "Hollister",
-// 		"price": 750000,
-// 		"color": "biru muda",
-// 		"size": ["S", "M", "L"],
-// 		"description": "Kemeja flanel dengan warna yang cerah, terbuat dari bahan flanel yang nyaman dan berkualitas tinggi.",
-// 		"condition": "baru",
-// 		"stock": 25,
-// 		"availability": {
-// 			"online": true,
-// 			"offline": true
-// 		}
-// });
-
-// product.save()
-//     .then((result) => {
-//         console.log('Product saved:', result);
-//     })
-//     .catch((err) => {
-//         console.log('error', err);
-//     });
-
-// Product.findOneAndUpdate({name: 'Kemeja Flanel'}, {
-// 		"name": "Kemeja Flanel",
-// 		"brand": "Hollister",
-// 		"price": 150000,
-// 		"color": "biru muda",
-// 		"size": ["S", "M", "L"],
-// 		"description": "Kemeja flanel dengan warna yang cerah, terbuat dari bahan flanel yang nyaman dan berkualitas tinggi.",
-// 		"condition": "baru",
-// 		"stock": 10,
-// 		"availability": {
-// 			"online": true,
-// 			"offline": true
-// 		}
-// }, 
-
-// {new: true, runValidators: true}).then((result) => {
-//     console.log('Product updated:', result);
-// }).catch((err) => {
-//     console.log(err.errors.stock.properties.message);
-// });
